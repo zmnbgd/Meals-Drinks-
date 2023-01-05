40b5079f16c1cac32ad30df21b7d86875f6af3bc
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-01-05 14:12:22 +0100

    Add files via upload

--- DrinkDetailViewController.swift
+++ DrinkDetailViewController.swift
@@ -12,6 +12,27 @@ import SwiftyJSON
 
 class DrinkDetailViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var drinkImage: UIImageView!
+    @IBOutlet weak var drinkCategory: UILabel!
+    @IBOutlet weak var drinkInstructions: UILabel!
+    @IBOutlet weak var drinkGlass: UILabel!
+    @IBOutlet weak var drinkIngredient1: UILabel!
+    @IBOutlet weak var drinkIngredient2: UILabel!
+    @IBOutlet weak var drinkIngredient3: UILabel!
+    @IBOutlet weak var drinkIngredient4: UILabel!
+    @IBOutlet weak var drinkIngredient5: UILabel!
+    @IBOutlet weak var drinkIngredient6: UILabel!
+    @IBOutlet weak var drinkMeasure1: UILabel!
+    @IBOutlet weak var drinkMeasure2: UILabel!
+    @IBOutlet weak var drinkMeasure3: UILabel!
+    @IBOutlet weak var drinkMeasure4: UILabel!
+    @IBOutlet weak var drinkMeasure5: UILabel!
+    @IBOutlet weak var drinkMeasure6: UILabel!
+    
+    var selectedDrink: String?
+    var randomDrink: String?
+    var isRandom: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
