35c3374f8fde16568f8089177158faca0ba51093
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-06 01:09:18 +0100

    Add files via upload

--- DrinkDetailViewController.swift
+++ DrinkDetailViewController.swift
@@ -1,8 +1,8 @@
 //
 //  DrinkDetailViewController.swift
-//  MealsAndDrinksApp
+//  ProjectMeals&DrinksAppVer1.2
 //
-//  Created by Marko Zivanovic on 5.12.22..
+//  Created by Marko Zivanovic on 8.1.23..
 //
 
 import UIKit
@@ -17,19 +17,21 @@ class DrinkDetailViewController: UIViewController {
     @IBOutlet weak var drinkCategory: UILabel!
     @IBOutlet weak var drinkInstructions: UILabel!
     @IBOutlet weak var drinkGlass: UILabel!
-    @IBOutlet weak var drinkIngredient: UILabel!
-    @IBOutlet weak var drinkMeasure: UILabel!
+    @IBOutlet weak var drinkIngredient1: UILabel!
+    @IBOutlet weak var drinkIngredient2: UILabel!
+    @IBOutlet weak var drinkIngredient3: UILabel!
+    @IBOutlet weak var drinkIngredient4: UILabel!
+    @IBOutlet weak var drinkIngredient5: UILabel!
+    @IBOutlet weak var drinkMeasure1: UILabel!
     @IBOutlet weak var drinkMeasure2: UILabel!
     @IBOutlet weak var drinkMeasure3: UILabel!
     @IBOutlet weak var drinkMeasure4: UILabel!
     @IBOutlet weak var drinkMeasure5: UILabel!
-    @IBOutlet weak var drinkMeasure6: UILabel!
-    @IBOutlet weak var drinkMeasure7: UILabel!
     
-   var selectedDrink: Drinks?
-   var randomDrink: Drinks?
-   var isRandom: Bool = false
-   var drinks3: [Drinks] = []
+    var selectedDrink: Drinks?
+    var randomDrink: Drinks?
+    var isRandom: Bool = false
+    var drinks3: [Drinks] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,57 +39,66 @@ class DrinkDetailViewController: UIViewController {
         setUp()
         multipleLineInstructions()
         if isRandom {
-            searchRandomCoctail()
-        } 
+            setUpRandomCoctail()
+        }
     }
+    
+   //MARK: Presenting Data in DrinkDetailVC
     private func setUpData() {
-        title = selectedDrink?.strCategory
+        title = selectedDrink?.strDrink
         drinkCategory.text = selectedDrink?.strCategory
         drinkInstructions.text = selectedDrink?.strInstructions
         drinkGlass.text = selectedDrink?.strGlass
-        drinkIngredient.text = selectedDrink?.strIngredient1
-        drinkMeasure.text = selectedDrink?.strMeasure1
+        drinkIngredient1.text = selectedDrink?.strIngredient1
+        drinkIngredient2.text = selectedDrink?.strIngredient2
+        drinkIngredient3.text = selectedDrink?.strIngredient3
+        drinkIngredient4.text = selectedDrink?.strIngredient4
+        drinkIngredient5.text = selectedDrink?.strIngredient5
+        drinkMeasure1.text = selectedDrink?.strMeasure1
         drinkMeasure2.text = selectedDrink?.strMeasure2
         drinkMeasure3.text = selectedDrink?.strMeasure3
         drinkMeasure4.text = selectedDrink?.strMeasure4
         drinkMeasure5.text = selectedDrink?.strMeasure5
-        drinkMeasure6.text = selectedDrink?.strMeasure6
-        drinkMeasure7.text = selectedDrink?.strMeasure7
         if let url = URL(string: selectedDrink?.strDrinkThumb ?? "") {
             drinkImage.kf.setImage(with: url)
         }
     }
     
-    private func setUpRandom() {
+    //MARK: Presenting Data in DrinkDetailVC For Random Coctail
+    private func setUpRandomCoctailData() {
         title = drinks3.first?.strCategory
-        drinkCategory.text = drinks3.first?.strCategory
         drinkInstructions.text = drinks3.first?.strInstructions
         drinkGlass.text = drinks3.first?.strGlass
-        drinkIngredient.text = drinks3.first?.strIngredient1
-        drinkMeasure.text = drinks3.first?.strMeasure1
+        drinkIngredient1.text = drinks3.first?.strIngredient1
+        drinkIngredient2.text = selectedDrink?.strIngredient2
+        drinkIngredient3.text = selectedDrink?.strIngredient3
+        drinkIngredient4.text = selectedDrink?.strIngredient4
+        drinkIngredient5.text = selectedDrink?.strIngredient5
+        drinkMeasure1.text = drinks3.first?.strMeasure1
         drinkMeasure2.text = drinks3.first?.strMeasure2
         drinkMeasure3.text = drinks3.first?.strMeasure3
         drinkMeasure4.text = drinks3.first?.strMeasure4
         drinkMeasure5.text = drinks3.first?.strMeasure5
-        drinkMeasure6.text = drinks3.first?.strMeasure6
-        drinkMeasure7.text = drinks3.first?.strMeasure7
         if let url = URL(string: drinks3.first?.strDrinkThumb ?? "") {
             drinkImage.kf.setImage(with: url)
         }
     }
     
-    
-    private func setupRandomDrink() {
+    //MARK: SetUp Random Coctail Name
+    private func setUpRandomCoctail() {
         drinkCategory.text = selectedDrink?.strCategory
     }
     
     private func setUp() {
         scrollView.isScrollEnabled = true
     }
+    
+    //MARK: SetUp Random Coctail
     private func multipleLineInstructions() {
         drinkInstructions.numberOfLines = 0
     }
 }
+//MARK: Get API Call For Random Coctail Search
 extension DrinkDetailViewController {
     func searchRandomCoctail() {
         let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
@@ -96,8 +107,8 @@ extension DrinkDetailViewController {
             case .success:
                 print(response.result)
                 do {
-                   let myresult = try? JSON(data: response.data!)
-                    for item in myresult!["drinks"].arrayValue {
+                    let myRandomCoctailResult = try? JSON(data: response.data!)
+                    for item in myRandomCoctailResult!["drinks"].arrayValue {
                         let name = item["strCategory"].stringValue
                         let instruction = item["strInstructions"].stringValue
                         let glass = item["strGlass"].stringValue
@@ -108,21 +119,18 @@ extension DrinkDetailViewController {
                         let ingredient4 = item["strIngredient4"].stringValue
                         let ingredient5 = item["strIngredient5"].stringValue
                         let ingredient6 = item["strIngredient6"].stringValue
-                        let ingredient7 = item["strIngredient7"].stringValue
                         let measure1 = item["strMeasure1"].stringValue
                         let measure2 = item["strMeasure2"].stringValue
                         let measure3 = item["strMeasure3"].stringValue
                         let measure4 = item["strMeasure4"].stringValue
                         let measure5 = item["strMeasure5"].stringValue
                         let measure6 = item["strMeasure6"].stringValue
-                        let measure7 = item["strMeasure7"].stringValue
-                        self.drinks3.append(Drinks.init(strCategory: name, strInstructions: instruction, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strIngredient7: ingredient7, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6, strMeasure7: measure7))
+                        self.drinks3.append(Drinks.init(strCategory: name, strInstructions: instruction, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6))
                     }
-                    self.setUpRandom()
                 }
             case .failure:
                 let alert = UIAlertView()
-                alert.title = "No internet conection!"
+                alert.title = "There is a problem with internet connection"
                 alert.show()
             }
         }
--- DrinksViewController.swift
+++ DrinksViewController.swift
@@ -1,3 +1,9 @@
+//
+//  DrinksViewController.swift
+//  ProjectMeals&DrinksAppVer1.2
+//
+//  Created by Marko Zivanovic on 6.1.23..
+//
 
 import UIKit
 import Alamofire
@@ -5,8 +11,9 @@ import SwiftyJSON
 
 class DrinksViewController: UIViewController {
     
-    @IBOutlet weak var drinksCollectionView: UICollectionView!
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var drinksCollectionView: UICollectionView!
+    
     
     var data: [DrinksModel] = []
     var drinks: [Drinks] = [] {
@@ -14,15 +21,6 @@ class DrinksViewController: UIViewController {
             self.drinksCollectionView.reloadData()
         }
     }
-    var drinks2: [Drinks] = [] {
-        didSet {
-            self.drinksCollectionView.reloadData()
-        }
-    }
-    
-    var drinks3: [Drinks] = []
-
-    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp()
@@ -31,31 +29,20 @@ class DrinksViewController: UIViewController {
     @objc func searchCoctailByName(_ textField: UITextField) {
         searchDrinksName(coctail: searchTextField.text ?? "")
     }
-    
-    //MARK: Random Coctail Button 
-    @IBAction func randomCoctail(_ sender: UIButton) {
-       // print("radom coctail")
-        if let dvc = storyboard?.instantiateViewController(withIdentifier: "DrinkDetailViewController") as? DrinkDetailViewController {
-            dvc.isRandom = true
-            self.navigationController?.pushViewController(dvc, animated: true)
-        }
-    }
-}
 
-//MARK: - Private Methods
+}
+//MARK: Private Methods
 extension DrinksViewController {
     private func setUp() {
-        setupDelegate()
+        setUpDelegate()
         setUpCollection()
         drinkFetch()
-        searchTextField.addTarget(self, action: #selector(searchCoctailByName), for: .editingChanged)
+
     }
-    
-    private func setupDelegate() {
+    private func setUpDelegate() {
         drinksCollectionView.delegate = self
         drinksCollectionView.dataSource = self
     }
-    
     private func setUpCollection() {
         let nib = UINib(nibName: DrinkCollectionViewCell.identifier, bundle: nil)
         drinksCollectionView!.register(nib, forCellWithReuseIdentifier: DrinkCollectionViewCell.identifier)
@@ -63,11 +50,10 @@ extension DrinksViewController {
     }
 }
 
-// MARK: Interaction with the cell
+//MARK: Interaction with Cell
 extension DrinksViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
-        // print("Tap")
         let selectedDrink = drinks[indexPath.row]
         if let dvc = storyboard?.instantiateViewController(withIdentifier: "DrinkDetailViewController") as? DrinkDetailViewController {
             dvc.selectedDrink = selectedDrink
@@ -76,12 +62,11 @@ extension DrinksViewController: UICollectionViewDelegate {
     }
 }
 
-//MARK: - UICollectionViewDataSource
-extension DrinksViewController: UICollectionViewDataSource {
+//MARK: UICollection View Data Source
+extension DrinksViewController: UICollectionViewDataSource { 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return drinks.count
     }
-
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DrinkCollectionViewCell.identifier, for: indexPath) as? DrinkCollectionViewCell else {
             return UICollectionViewCell()
@@ -90,6 +75,7 @@ extension DrinksViewController: UICollectionViewDataSource {
         cell.setupCell(data: item)
         return cell
     }
+    
 }
 
 //MARK: Get API Call With Alamofire
@@ -101,11 +87,12 @@ extension DrinksViewController {
             case .success:
                 print(response.result)
                 self.drinks.removeAll()
-                do {
-                   let myresult = try? JSON(data: response.data!)
+                do{
+                    let myresult = try? JSON(data: response.data!)
                     for item in myresult!["drinks"].arrayValue {
-                        let name = item["strCategory"].stringValue
-                        let instruction = item["strInstructions"].stringValue
+                        let name = item["strDrink"].stringValue
+                        let category = item["strCategory"].stringValue
+                        let instructions = item["strInstructions"].stringValue
                         let glass = item["strGlass"].stringValue
                         let drinkThumb = item["strDrinkThumb"].stringValue
                         let ingredient1 = item["strIngredient1"].stringValue
@@ -114,17 +101,14 @@ extension DrinksViewController {
                         let ingredient4 = item["strIngredient4"].stringValue
                         let ingredient5 = item["strIngredient5"].stringValue
                         let ingredient6 = item["strIngredient6"].stringValue
-                        let ingredient7 = item["strIngredient7"].stringValue
                         let measure1 = item["strMeasure1"].stringValue
                         let measure2 = item["strMeasure2"].stringValue
                         let measure3 = item["strMeasure3"].stringValue
                         let measure4 = item["strMeasure4"].stringValue
                         let measure5 = item["strMeasure5"].stringValue
                         let measure6 = item["strMeasure6"].stringValue
-                        let measure7 = item["strMeasure7"].stringValue
-                        self.drinks.append(Drinks.init(strCategory: name, strInstructions: instruction, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strIngredient7: ingredient7, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6, strMeasure7: measure7))
+                        self.drinks.append(Drinks.init(strDrink: name, strCategory: category, strInstructions: instructions, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6))
                     }
-                    
                 }
             case .failure:
                 let alert = UIAlertView()
@@ -135,7 +119,7 @@ extension DrinksViewController {
     }
 }
 
-//MARK: CollectioView Cell GridLayout
+//MARK: Collection Cell GridLayout
 extension DrinksViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(
            _ collectionView: UICollectionView,
@@ -152,22 +136,21 @@ extension DrinksViewController: UICollectionViewDelegateFlowLayout {
            let itemHeight = itemWidth * 1.3
            return CGSize(width: itemWidth, height: itemHeight)
        }
-    
 }
 
-//MARK: Get API for Coctail Search
-extension DrinksViewController {    
-    func searchDrinksName(coctail: String?) {
+//MARK: Get API For Search Coctail
+extension DrinksViewController {
+    func searchDrinksName(coctail: String) {
         let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
         let finalUrl = url + (coctail ?? "")
-        Alamofire.request(finalUrl, method: .get).responseJSON { (response) in
+        Alamofire.request(url, method: .get).responseJSON { (response) in
             switch response.result {
             case .success:
                 print(response.result)
                 self.drinks.removeAll()
                 do {
-                   let myresult = try? JSON(data: response.data!)
-                    for item in myresult!["drinks"].arrayValue {
+                    let myResult = try? JSON(data: response.data!)
+                    for item in myResult!["drinks"].arrayValue {
                         let name = item["strCategory"].stringValue
                         let instruction = item["strInstructions"].stringValue
                         let glass = item["strGlass"].stringValue
@@ -178,79 +161,20 @@ extension DrinksViewController {
                         let ingredient4 = item["strIngredient4"].stringValue
                         let ingredient5 = item["strIngredient5"].stringValue
                         let ingredient6 = item["strIngredient6"].stringValue
-                        let ingredient7 = item["strIngredient7"].stringValue
                         let measure1 = item["strMeasure1"].stringValue
                         let measure2 = item["strMeasure2"].stringValue
                         let measure3 = item["strMeasure3"].stringValue
                         let measure4 = item["strMeasure4"].stringValue
                         let measure5 = item["strMeasure5"].stringValue
                         let measure6 = item["strMeasure6"].stringValue
-                        let measure7 = item["strMeasure7"].stringValue
-                        self.drinks.append(Drinks.init(strCategory: name, strInstructions: instruction, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strIngredient7: ingredient7, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6, strMeasure7: measure7))
+                        self.drinks.append(Drinks.init(strCategory: name, strInstructions: instruction, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6))
                     }
-
                 }
             case .failure:
                 let alert = UIAlertView()
-                alert.title = "No internet conection!"
+                alert.title = "Something went wrong, check API call"
                 alert.show()
             }
         }
     }
 }
-
-//MARK: Get API Call From Random Coctail
-extension DrinksViewController {
-    func searchRandomCoctail() {
-        let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-        Alamofire.request(url, method: .get).responseJSON { (response) in
-            switch response.result {
-            case .success:
-                print(response.result)
-                self.drinks3.removeAll()
-                do {
-                   let myresult = try? JSON(data: response.data!)
-                    for item in myresult!["drinks"].arrayValue {
-                        let name = item["strCategory"].stringValue
-                        let instruction = item["strInstructions"].stringValue
-                        let glass = item["strGlass"].stringValue
-                        let drinkThumb = item["strDrinkThumb"].stringValue
-                        let ingredient1 = item["strIngredient1"].stringValue
-                        let ingredient2 = item["strIngredient2"].stringValue
-                        let ingredient3 = item["strIngredient3"].stringValue
-                        let ingredient4 = item["strIngredient4"].stringValue
-                        let ingredient5 = item["strIngredient5"].stringValue
-                        let ingredient6 = item["strIngredient6"].stringValue
-                        let ingredient7 = item["strIngredient7"].stringValue
-                        let measure1 = item["strMeasure1"].stringValue
-                        let measure2 = item["strMeasure2"].stringValue
-                        let measure3 = item["strMeasure3"].stringValue
-                        let measure4 = item["strMeasure4"].stringValue
-                        let measure5 = item["strMeasure5"].stringValue
-                        let measure6 = item["strMeasure6"].stringValue
-                        let measure7 = item["strMeasure7"].stringValue
-                        self.drinks3.append(Drinks.init(strCategory: name, strInstructions: instruction, strGlass: glass, strDrinkThumb: drinkThumb, strIngredient1: ingredient1, strIngredient2: ingredient2, strIngredient3: ingredient3, strIngredient4: ingredient4, strIngredient5: ingredient5, strIngredient6: ingredient6, strIngredient7: ingredient7, strMeasure1: measure1, strMeasure2: measure2, strMeasure3: measure3, strMeasure4: measure4, strMeasure5: measure5, strMeasure6: measure6, strMeasure7: measure7))
-                    }
-
-                }
-            case .failure:
-                let alert = UIAlertView()
-                alert.title = "No internet conection!"
-                alert.show()
-            }
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
